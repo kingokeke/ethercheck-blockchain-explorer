cbdc7950b0672c77f3ce2f6a1ba7df3289d882f2
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-02-21 04:10:39 +0100

    feature: created header component

---
+++ components/layout/header.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/Header.module.css'
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>This is the site header</h1>
+    </header>
+  )
+}
+
+export default Header
