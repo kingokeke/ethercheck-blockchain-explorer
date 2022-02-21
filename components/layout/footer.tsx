ead1579a2f0788f1c1539379dc4828e3d0ed08a1
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-02-21 04:12:56 +0100

    feature: created site footer component

---
+++ components/layout/footer.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/Footer.module.css'
+
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>This is the site footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
