7e820f9f7578bbcc4ab747864bae553e90b0419e
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-02-22 06:22:48 +0100

    feature: created nav link

---
+++ components/navLink.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import NavLinkProps from "../models/navLink.model";
+import styles from "../styles/navItem.module.scss";
+
+const NavLink = ({ href, title }: NavLinkProps): JSX.Element => (
+  <Link href={href}>
+    <a className={styles.navMenuItem}>{title}</a>
+  </Link>
+);
+
+export default NavLink;
---
+++ models/navLink.model.ts
@@ -0,0 +1,4 @@
+export default interface NavLinkProps {
+  href: string;
+  title: string;
+}
