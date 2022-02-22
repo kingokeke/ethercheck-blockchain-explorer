b33d2e31d4ba3908b3f5b9dccc604395b2efb301
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-02-22 06:49:50 +0100

    feature: added markup for header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,11 +1,42 @@
-import styles from '../../styles/Header.module.css'
+import NavLinkProps from "../../models/navLink.model";
+import styles from "../../styles/header.module.scss";
+import NavLink from "../navLink";
+import SiteContainer from "./container";
+
+const menuItems: NavLinkProps[] = [
+  { href: "#", title: "Home" },
+  { href: "#", title: "Block Explorer" },
+  { href: "#", title: "Tokens" },
+  { href: "#", title: "Resources" },
+  { href: "#", title: "More" },
+];
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>This is the site header</h1>
+      <SiteContainer>
+        <div className={styles.header__content}>
+          <h1 className={styles.header__logo}>
+            EtherCheck<span className={styles.header__logo_icon}>🔎</span>
+          </h1>
+          <nav className={styles.header__navigation}>
+            <ul className={styles.header__navigation_links}>
+              {menuItems.map((item, index) => (
+                <li key={index} className={styles.header__navigation_item}>
+                  <NavLink href={item.href} title={item.title} />
+                </li>
+              ))}
+              <li className={styles.header__navigation_item}>
+                <a href="#" className={styles.header__signin}>
+                  Sign In
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </SiteContainer>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
