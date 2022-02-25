c985d78d70071458213a85c945d419ef87904900
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-02-25 23:54:26 +0100

    refactor: reorganized layout in root component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Header from '../components/layout/header';
+import Footer from '../components/layout/footer';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>EtherCheck | Ethereum Explorer</title>
+        <meta name="description" content="EtherCheck🔎 is an alternative ethereum blockchain explorer" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
 
 export default MyApp
